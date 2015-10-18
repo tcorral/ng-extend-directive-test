b88f112adbd42945d95b1e31d09276eab3b909aa
tomascorralcasas <amischol@gmail.com>
2015-10-18 23:47:18 +0200

    Move directive to load the app to the main.js file.

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,21 @@
 require.config({
   baseUrl: 'scripts'
 });
+angular.module('preApp', [])
+  .directive('gOnLoad', function () {
+    return {
+      restrict: 'A',
+      scope: {},
+      link: function (scope, element, attrs) {
+        var content = angular.element(document.body).html();
+        require([attrs.gOnLoad], function (module) {
+          angular.element(document.body).html('<div ng-controller="' +  module.name + 'Controller as ' + module.aliasController + '">' + content + '</div>');
+          angular.bootstrap(angular.element(document.body).find('div')[0], [module.name]);
+          document.body.removeAttribute('ng-cloak');
+        });
+      }
+    }
+  });
+angular.element(document).ready(function () {
+  angular.bootstrap(document.getElementsByTagName('head')[0], ['preApp']);
+});
